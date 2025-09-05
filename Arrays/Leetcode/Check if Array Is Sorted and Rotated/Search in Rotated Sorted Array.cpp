531186bd7823f63a0921de73623abec157c25905
Junaid Bin Jahangir <164465375+junaidr04@users.noreply.github.com>
2025-09-05 12:29:03 +0600

    Add files via upload

---
+++ Arrays/Leetcode/Check if Array Is Sorted and Rotated/Search in Rotated Sorted Array.cpp
@@ -0,0 +1,38 @@
+class Solution
+{
+public:
+    int search(vector<int> &nums, int target)
+    {
+        int s = 0;
+        int e = nums.size() - 1;
+        while (s <= e)
+        {
+            int mid = (s + e) / 2;
+            if (target == nums[mid])
+                return mid;
+            if (nums[s] <= nums[mid])
+            {
+                if (target >= nums[s] && target < nums[mid])
+                {
+                    e = mid - 1;
+                }
+                else
+                {
+                    s = mid + 1;
+                }
+            }
+            else
+            {
+                if (target <= nums[e] && target > nums[mid])
+                {
+                    s = mid + 1;
+                }
+                else
+                {
+                    e = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+};
