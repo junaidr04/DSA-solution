4817e1e04a3df888e465f833368bd662ddaf82e3
Junaid Bin Jahangir <164465375+junaidr04@users.noreply.github.com>
2025-09-09 00:42:18 +0600

    Add files via upload

---
+++ Arrays/Leetcode/Longest Common Prefix.cpp
@@ -0,0 +1,25 @@
+class Solution
+{
+public:
+    string longestCommonPrefix(vector<string> &strs)
+    {
+        if (strs.empty())
+            return "";
+        string pref = strs[0];
+        int prefLen = pref.length();
+        for (int i = 0; i < strs.size(); i++)
+        {
+            string s = strs[i];
+            while (prefLen > s.size() || pref != s.substr(0, prefLen))
+            {
+                prefLen--;
+                if (prefLen == 0)
+                {
+                    return "";
+                }
+                pref = pref.substr(0, prefLen);
+            }
+        }
+        return pref;
+    }
+};
