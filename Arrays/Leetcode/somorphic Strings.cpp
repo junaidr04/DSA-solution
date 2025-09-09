4a3055eb04a1beaeba56aa1f780408db4c6cacb5
Junaid Bin Jahangir <164465375+junaidr04@users.noreply.github.com>
2025-09-09 19:44:36 +0600

    Add files via upload

---
+++ Arrays/Leetcode/somorphic Strings.cpp
@@ -0,0 +1,39 @@
+class Solution
+{
+public:
+    bool isIsomorphic(string s, string t)
+    {
+        if (s.size() != t.size())
+            return false;
+        unordered_map<char, char> map1;
+        unordered_map<char, char> map2;
+        for (int i = 0; i < s.size(); i++)
+        {
+            char c1 = s[i];
+            char c2 = t[i];
+            if (map1.count(c1))
+            {
+                if (map1[c1] != c2)
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                map1[c1] = c2;
+            }
+            if (map2.count(c2))
+            {
+                if (map2[c2] != c1)
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                map2[c2] = c1;
+            }
+        }
+        return true;
+    }
+};
