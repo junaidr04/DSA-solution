37e8f4cf1a17dc57be7d626bb9da22b44e8119a8
Junaid Bin Jahangir <164465375+junaidr04@users.noreply.github.com>
2025-09-15 21:24:47 +0600

    Add files via upload

---
+++ Searching and Sorting/Search Insert Position.cpp
@@ -0,0 +1,25 @@
+class Solution
+{
+public:
+    int searchInsert(vector<int> &nums, int target)
+    {
+        int s = 0, e = nums.size() - 1;
+        while (s <= e)
+        {
+            int mid = (s + e) / 2;
+            if (nums[mid] == target)
+            {
+                return mid;
+            }
+            if (target > nums[mid])
+            {
+                s = mid + 1;
+            }
+            else
+            {
+                e = mid - 1;
+            }
+        }
+        return s;
+    }
+};
