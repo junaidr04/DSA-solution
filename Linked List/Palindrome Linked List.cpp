c6eedf1b137bc940f0d31dd4fd30dfd994fda40d
Junaid Bin Jahangir <164465375+junaidr04@users.noreply.github.com>
2025-10-03 10:16:25 +0600

    Add files via upload

---
+++ Linked List/Palindrome Linked List.cpp
@@ -0,0 +1,52 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution
+{
+public:
+    bool isPalindrome(ListNode *head)
+    {
+        if (!head)
+            return true;
+
+        // two pointer(find middle)
+        ListNode *slow = head;
+        ListNode *fast = head;
+        while (fast && fast->next)
+        {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        // Reverse 2nd half
+        ListNode *prev = NULL;
+        while (slow)
+        {
+            ListNode *nxt = slow->next;
+            slow->next = prev;
+            prev = slow;
+            slow = nxt;
+        }
+
+        // compare two halves
+        ListNode *left = head;
+        ListNode *right = prev;
+        while (right)
+        {
+            if (left->val != right->val)
+            {
+                return false;
+            }
+            left = left->next;
+            right = right->next;
+        }
+        return true;
+    }
+};
