4880a12f435c39237391440a88ac8a470773c33e
Junaid Bin Jahangir <164465375+junaidr04@users.noreply.github.com>
2025-10-05 12:28:33 +0600

    Add files via upload

---
+++ Stack/Remove All Adjacent Duplicates In String.cpp
@@ -0,0 +1,27 @@
+class Solution
+{
+public:
+    string removeDuplicates(string s)
+    {
+        stack<char> st;
+        for (char ch : s)
+        {
+            if (!st.empty() && st.top() == ch)
+            {
+                st.pop();
+            }
+            else
+            {
+                st.push(ch);
+            }
+        }
+        string result = "";
+        while (!st.empty())
+        {
+            result += st.top();
+            st.pop();
+        }
+        reverse(result.begin(), result.end());
+        return result;
+    }
+};
