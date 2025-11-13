aabd1b3a1dbefb84b1100752e04c053f90dce39a
Junaid Bin Jahangir <164465375+junaidr04@users.noreply.github.com>
2025-11-13 12:35:42 +0600

    Add files via upload

---
+++ Tree/Binary Tree Zigzag Level Order Traversal.cpp
@@ -0,0 +1,51 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution
+{
+public:
+    vector<vector<int>> zigzagLevelOrder(TreeNode *root)
+    {
+        vector<vector<int>> ans;
+        if (root == NULL)
+            return ans;
+        queue<TreeNode *> q;
+        q.push(root);
+        bool leftToright = true;
+        while (!q.empty())
+        {
+            int s = q.size();
+            vector<int> level(s);
+            for (int i = 0; i < s; i++)
+            {
+                TreeNode *node = q.front();
+                q.pop();
+                int index;
+                if (leftToright)
+                {
+                    index = i;
+                }
+                else
+                {
+                    index = s - 1 - i;
+                }
+                level[index] = node->val;
+                if (node->left)
+                    q.push(node->left);
+                if (node->right)
+                    q.push(node->right);
+            }
+            ans.push_back(level);
+            leftToright = !leftToright;
+        }
+        return ans;
+    }
+};
---
+++ Tree/Flatten Binary Tree to Linked List.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution
+{
+public:
+    void flatten(TreeNode *root)
+    {
+        if (root == NULL)
+            return;
+        flatten(root->left);
+        flatten(root->right);
+        TreeNode *Right = root->right;
+        root->right = root->left;
+        root->left = NULL;
+        TreeNode *cur = root;
+        while (cur->right != NULL)
+        {
+            cur = cur->right;
+        }
+        cur->right = Right;
+    }
+};
