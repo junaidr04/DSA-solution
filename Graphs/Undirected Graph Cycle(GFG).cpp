ea300b9b0299e0696b2bf39fdd291f3885ee6831
Junaid Bin Jahangir <164465375+junaidr04@users.noreply.github.com>
2025-11-19 19:59:40 +0600

    Add files via upload

---
+++ Graphs/Undirected Graph Cycle(GFG).cpp
@@ -0,0 +1,38 @@
+class Solution
+{
+public:
+    bool dfs(int node, int parent, vector<vector<int>> &adj, vector<int> &vis)
+    {
+        vis[node] = 1;
+        for (int nxt : adj[node])
+        {
+            if (nxt == parent)
+                continue;
+            if (vis[nxt])
+                return true;
+            if (dfs(nxt, node, adj, vis))
+                return true;
+        }
+        return false;
+    }
+    bool isCycle(int V, vector<vector<int>> &edges)
+    {
+        vector<vector<int>> adj(V);
+        for (auto &e : edges)
+        {
+            int u = e[0], v = e[1];
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+        vector<int> vis(V, 0);
+        for (int i = 0; i < V; i++)
+        {
+            if (!vis[i])
+            {
+                if (dfs(i, -1, adj, vis))
+                    return true;
+            }
+        }
+        return false;
+    }
+};
